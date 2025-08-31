1222910334e2ea617b4469e14f3841b14aa00136
Shivam Mishra <ishivammishra2003@gmail.com>
2025-08-31 22:57:32 +0530

    Third commit

---
+++ src/exception.py
@@ -0,0 +1,13 @@
+import sys
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exe_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+
+    )
+
+class CustomException(Exception):
+    def _init_(self,error_message,error_detail:sys):
+        super.__init__(error_message)
